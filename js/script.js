9947d9e64f779398c832f20600ccd7b408f49a96
末満　堅人 <suemiken@icloud.com>
2017-07-10 21:08:15 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿$(function() {
+   $('.slide_type_a').slick({
+	fade: true,
+	cssEase: 'linear',
+	arrows: false,
+	lazyLoad: "progressive"
+	});
+});
+
+$(function() {
+    $(window).scroll(function() {
+            var dy = $(this).scrollTop();
+            $('#nav').css('top', dy < 80 ? 80-dy : 0);
+    });
+});
